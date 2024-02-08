8b9017f7894f267270f82758d74f00d350aa059e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-08 14:36:46 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/blog/blogArtical.tsx
+++ src/blog/blogArtical.tsx
@@ -1,5 +1,6 @@
 import IonIcon from "@reacticons/ionicons"
 
+
 const blogArtical = () => {
   return (
     <div className="container mx-auto px-16">
@@ -14,7 +15,12 @@ const blogArtical = () => {
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus eaque dignissimos laborum iure aut quaerat, libero, nostrum repudiandae quibusdam dolorem nobis nihil voluptas fugit assumenda iste totam quae pariatur quia!
             <p className="">Awareness</p>
             <p className="">Stop the next pandemic? Stop illegal trade!</p>
-            <p className=""><a href="" className="">Read Article <IonIcon name="arrow-forward" /></a></p>
+            <div className="flex">
+            <a href="" className="">
+                <span className="">Read Article</span>
+                <span ><IonIcon className="pl-4" name="arrow-forward"/></span>
+            </a>
+            </div>
         </div>
         <div>
             <div className="bg-cover bg-center h-80 w-96" style={{backgroundImage:"url('https://www.hdcarwallpapers.com/thumbs/2024/audi_q7_55_tfsi_quattro_s_line_2024-t1.jpg')"}}>
