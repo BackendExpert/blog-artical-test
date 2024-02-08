ffd747ee357547875c6d71e2a1af567bb36a4da1
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-08 21:04:35 +0530

    Update
    Co-Authored-By: JehanKandy <jehankandy@gmail.com>

--- src/blog/blogArtical.tsx
+++ src/blog/blogArtical.tsx
@@ -9,8 +9,12 @@ const blogArtical = () => {
         <p className="">Advancing the frontiers of fundamental research</p>
       </div>
       <div className="grid grid-cols-2 gap-4">
-        <div>div</div>
-        <div>div</div>
+        <div>
+          <img src="" alt="" className="" />
+        </div>
+        <div>
+          <img src="" alt="" className="" />
+        </div>
       </div>
     </div>
   )
