dd23c9eeec37f3a57fab05ffdc6444657ed4a2f4
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-08 21:45:27 +0530

    Update
    Co-Authored-By: JehanKandy <jehankandy@gmail.com>

--- src/blog/blogArtical.tsx
+++ src/blog/blogArtical.tsx
@@ -8,22 +8,21 @@ const blogArtical = () => {
         <h1 className="text-4xl font-semibold py-2">Recent Scientific Publication</h1>
         <p className="text-xl">Advancing the frontiers of fundamental research</p>
       </div>
-      <div className="md:grid grid-cols-2 gap-16 my-8">
+      <div className="md:grid grid-cols-2 gap-16 mt-8 mb-36">
         <div className="md:my-0 my-8">
           <img src="https://wallpapercave.com/wp/wp2707503.jpg" alt="" className="h-full rounded" />
           <p className="mt-2 text-sm">Awareness</p>
           <p className="text-2xl">Stop the next Pandemic? Stop illegal trade!</p>
           <p className="text-blue-500"><a href="">Read Article <IonIcon name="arrow-forward" /></a></p>
         </div>
-        <div>
+        <div className="md:my-0 mt-8">
           <img src="https://wallpapercave.com/wp/wp2707510.jpg" alt="" className="h-full rounded" />
           <p className="mt-2 text-sm">Awareness</p>
           <p className="text-2xl">The Science of Aji-no-moto</p>
           <p className="text-blue-500"><a href="">Read Article <IonIcon name="arrow-forward" /></a></p>
         </div>
       </div>
-      
-      
+        <span className="">Read Blog Articles</span>
     </div>
   )
 }
