bc51821481d89176b5f39664a8ffe8cedb0b6f9d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-08 14:18:43 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/blog/blogArtical.tsx
+++ src/blog/blogArtical.tsx
@@ -14,7 +14,7 @@ const blogArtical = () => {
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus eaque dignissimos laborum iure aut quaerat, libero, nostrum repudiandae quibusdam dolorem nobis nihil voluptas fugit assumenda iste totam quae pariatur quia!
             <p className="">Awareness</p>
             <p className="">Stop the next pandemic? Stop illegal trade!</p>
-            <p className=""><a href="" className="">Read Article <IonIcon name="" /></a></p>
+            <p className=""><a href="" className="">Read Article <IonIcon name="arrow-forward" /></a></p>
         </div>
         <div>
             <div className="bg-cover bg-center h-80 w-96" style={{backgroundImage:"url('https://www.hdcarwallpapers.com/thumbs/2024/audi_q7_55_tfsi_quattro_s_line_2024-t1.jpg')"}}>
