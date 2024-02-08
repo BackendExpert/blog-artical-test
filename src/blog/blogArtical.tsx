b318c19e09bcfa66ebcd7fef16f1e2e3f73be7d0
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-08 14:14:45 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/blog/blogArtical.tsx
+++ src/blog/blogArtical.tsx
@@ -1,3 +1,5 @@
+
+
 const blogArtical = () => {
   return (
     <div className="container mx-auto px-16">
@@ -7,11 +9,18 @@ const blogArtical = () => {
     </div>
     <div className="md:grid md:grid-cols-2 gap-2 my-12">
         <div>
-            <div>
-
+            <div className="bg-cover bg-center h-80 w-96" style={{backgroundImage:"url('https://www.hdcarwallpapers.com/thumbs/2024/porsche_taycan_2024_4k-t1.jpg')"}}>
             </div>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus eaque dignissimos laborum iure aut quaerat, libero, nostrum repudiandae quibusdam dolorem nobis nihil voluptas fugit assumenda iste totam quae pariatur quia!
+            <p className="">Awareness</p>
+            <p className="">Stop the next pandemic? Stop illegal trade!</p>
+            <p className=""><a href="" className="">Read Article </a></p>
         </div>
-        <div>Div1</div>        
+        <div>
+            <div className="bg-cover bg-center h-80 w-96" style={{backgroundImage:"url('https://www.hdcarwallpapers.com/thumbs/2024/audi_q7_55_tfsi_quattro_s_line_2024-t1.jpg')"}}>
+                hi all
+            </div>
+        </div>        
     </div>
 
 
