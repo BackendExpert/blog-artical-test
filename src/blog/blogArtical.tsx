5a843742d51383da5c965bb4d0008ded1c8d530e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-08 15:06:18 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/blog/blogArtical.tsx
+++ src/blog/blogArtical.tsx
@@ -8,24 +8,16 @@ const blogArtical = () => {
         <h1 className="">Recent Scientific Publication</h1>
         <p className="">Advancing the frontiers of fundamental research</p>
     </div>
-    <div className="md:grid md:grid-cols-2 gap-20">
+    <div className="md:grid grid-cols-2 gap-4">
         <div>
-            <div className="bg-cover bg-center w-full h-full" style={{backgroundImage:"url('https://www.hdcarwallpapers.com/thumbs/2024/porsche_taycan_2024_4k-t1.jpg')"}}>
-            </div>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus eaque dignissimos laborum iure aut quaerat, libero, nostrum repudiandae quibusdam dolorem nobis nihil voluptas fugit assumenda iste totam quae pariatur quia!
-            <p className="">Awareness</p>
-            <p className="">Stop the next pandemic? Stop illegal trade!</p>
-            <a href="" className="flex">
-                <p>Read Article</p>
-                <span className="pt-[3px] pl-2"><IonIcon name="arrow-forward" /></span>
-            </a>
+             
+ 
+        <div className="h-96 bg-[url(https://placekitten.com/1400)] bg-no-repeat">
+        
         </div>
-        <div>
-            <div className="bg-cover bg-center w-full h-96" style={{backgroundImage:"url('https://www.hdcarwallpapers.com/thumbs/2024/audi_q7_55_tfsi_quattro_s_line_2024-t1.jpg')"}}>
-                hi all
-            </div>
-            
-        </div>        
+ 
+        </div>
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut animi, aliquid similique earum est odio, error adipisci laborum assumenda aperiam veniam debitis optio placeat fugit excepturi hic, quisquam asperiores eum?</div>
     </div>
 
 
