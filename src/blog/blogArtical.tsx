f4c82736c3403ed6ea9e73f77c24f80d41b1925a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-08 15:21:13 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/blog/blogArtical.tsx
+++ src/blog/blogArtical.tsx
@@ -8,12 +8,12 @@ const blogArtical = () => {
         <h1 className="">Recent Scientific Publication</h1>
         <p className="">Advancing the frontiers of fundamental research</p>
     </div>
-    <div className="md:grid grid-cols-2 gap-20">
+    <div className="md:grid grid-cols-2 gap-16 my-8">
         <div>
-            <img className="h-72 object-cover" src="https://c4.wallpaperflare.com/wallpaper/1022/53/128/ultra-wide-car-nissan-skyline-gt-r-wallpaper-preview.jpg" alt="" />
+            <img className="h-full object-cover" src="https://c4.wallpaperflare.com/wallpaper/1022/53/128/ultra-wide-car-nissan-skyline-gt-r-wallpaper-preview.jpg" alt="" />
         </div>
         <div>
-            <img className="h-72 object-cover" src="https://c4.wallpaperflare.com/wallpaper/956/868/1012/car-nissan-race-cars-road-wallpaper-preview.jpg" alt="" />
+            <img className="h-full object-cover" src="https://c4.wallpaperflare.com/wallpaper/956/868/1012/car-nissan-race-cars-road-wallpaper-preview.jpg" alt="" />
         </div>
     </div>
 
